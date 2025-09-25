bf7725e2f0e4cc456af006247f5e857d7b69ab46
pepegmaer2141-cmd <pepegmaer2141@gmail.com>
2025-09-25 04:52:21 -0300

    añadido spawn de balas

--- parry-desvio-1/scrip q spawnea balas.gd
+++ parry-desvio-1/scrip q spawnea balas.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 var pelotitas = preload("res://pelotitas.tscn").instantiate()
 @export var pelotitas_scene: PackedScene
-var spawn
+var spawny
 
 func _ready():
-	spawn = NodePath("juego/spawner/spawn:position")
+	spawny = NodePath("spawner/spawn:position")
 
 func _on_timer_timeout() -> void:
 	var enemy = pelotitas_scene.instantiate()
-	enemy.position = spawn
-	get_parent().add_child(enemy)
+	enemy.position = position
+	add_child(enemy)
