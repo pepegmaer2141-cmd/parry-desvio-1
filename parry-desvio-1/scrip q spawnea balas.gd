c6aba5b00c326777e76468b3447ed8f3b43c63c2
pepegmaer2141-cmd <pepegmaer2141@gmail.com>
2025-09-28 04:05:37 -0300

    pelotas no chocan entre si y se le spuede poner limite

--- parry-desvio-1/scrip q spawnea balas.gd
+++ parry-desvio-1/scrip q spawnea balas.gd
@@ -1,13 +1,15 @@
 extends Node2D
-
 var pelotitas = preload("res://pelotitas.tscn").instantiate()
 @export var pelotitas_scene: PackedScene
 var spawny
+var numerobala = 0
+var velomax = 500
+
 
 func _ready():
 	spawny = NodePath("spawner/spawn:position")
-
 func _on_timer_timeout() -> void:
+	numerobala=numerobala +1
 	var enemy = pelotitas_scene.instantiate()
 	enemy.position = position
-	add_child(enemy)
+	if numerobala < 15 : add_child(enemy)
